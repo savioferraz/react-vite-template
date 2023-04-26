5607390acb9fa5f9c147da5de20d0796746191df
savioferraz <savio.ferraz88@gmail.com>
2023-04-26 15:03:19 -0300

    template setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# React.js-Vite Template
+
+Template para aplicações front-end com React e Vite. Clone o repositório e rode o comando abaixo:
+
+```sh
+npm i
+```
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./assets/styles/reset.css";
+import "./assets/styles/globalStyle.css";
+
+function App() {
+  return (
+    <>
+      <h1>Blank</h1>
+    </>
+  );
+}
+
+export default App;
